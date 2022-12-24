6972464d42ebab60efc6277054b58100bc6a1344
kushagra <kushagratripathi7@gmail.com>
2022-12-25 04:15:22 +0530

    add: emojis slider

---
+++ src/assets/index.ts
@@ -0,0 +1,21 @@
+import emoji1 from "./Artboard 1.svg";
+import emoji2 from "./Artboard 2.svg";
+import emoji3 from "./Artboard 3.svg";
+import emoji4 from "./Artboard 4.svg";
+import emoji5 from "./Artboard 5.svg";
+import emoji6 from "./Artboard 6.svg";
+import emoji8 from "./Artboard 8.svg";
+import emoji9 from "./Artboard 9.svg";
+
+const emojisList = [
+  { id: 1, icon: emoji1, name: "Anxious and worried" },
+  { id: 2, icon: emoji2, name: "Relaxed" },
+  { id: 3, icon: emoji3, name: "Unhappy" },
+  { id: 4, icon: emoji4, name: "Angry" },
+  { id: 5, icon: emoji5, name: "Annoyed" },
+  { id: 6, icon: emoji6, name: "Very Excited" },
+  { id: 8, icon: emoji8, name: "Nauseated" },
+  { id: 9, icon: emoji9, name: "In Love" },
+];
+
+export default emojisList;
--- src/components/EmojiSelector/Emoji-selector.tsx
+++ src/components/EmojiSelector/Emoji-selector.tsx
@@ -1,5 +1,27 @@
+// Import Swiper styles
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import emojisList from "../../assets";
+
 const EmojiSelector = () => {
-  return <div className="emoji-selector"></div>;
+  return (
+    <Swiper
+      centeredSlides={true}
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {emojisList.map((emoji) => {
+        return (
+          <SwiperSlide key={emoji.id}>
+            <img src={emoji.icon} alt={emoji.name} />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default EmojiSelector;
