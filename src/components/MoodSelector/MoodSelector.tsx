be6d193aa9fa3be8d33f5af4bfd05247089a6a07
kushagra <kushagratripathi7@gmail.com>
2022-12-25 20:44:40 +0530

    fix: heading margin less

--- src/components/MoodSelector/MoodSelector.tsx
+++ src/components/MoodSelector/MoodSelector.tsx
@@ -4,10 +4,17 @@ interface MoodSelectorProps {
 }
 
 const MoodSelector = ({ feelingsList }: any) => {
+  const handleClick = () => {
+    // const grid = document.querySelector(".mood-list");
+    // const allCards = grid?.children;
+    // for (let i = 0; i < allCards?.length - 1; )
+    //   console.log("clicked the button", grid.children);
+  };
   return (
     <>
       <section className="mood-section">
         <h3 className="mood-section--title">Are you also feeling?</h3>
+        <button onClick={handleClick}>click me</button>
         <div className="mood-list ">
           {feelingsList.map((feeling: any) => {
             console.log(feeling);
